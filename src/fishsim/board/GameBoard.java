12373c216d1c6d77ad3718c5a8bf989b0d86462f
avabellaw <70448139+avabellaw@users.noreply.github.com>
2022-10-10 01:56:50 +0100

    added mouse input sortof

--- src/fishsim/board/GameBoard.java
+++ src/fishsim/board/GameBoard.java
@@ -9,29 +9,17 @@ import fishsim.entities.FishingLine;
 import fishsim.entities.GameHook;
 import fishsim.graphics.StaticSprites;
 
-public class GameBoard extends Board implements MouseMotionListener{
+public class GameBoard extends Board{
 
 	private GameHook gHook;
+	public static FishingLine line;
 
 	public GameBoard(Dimension boardSize, Display display) {
 		super(boardSize, display, StaticSprites.gameBlueprintSprite);
 		gHook = new GameHook((BOARD_SIZE.width / 2) - GameHook.WIDTH / 2, BOARD_SIZE.height - GameHook.HEIGHT);
 		entities.add(gHook);
-		FishingLine line = new FishingLine(gHook);
+		line = new FishingLine(gHook);
 		entities.add(line);
-	}
-
-	@Override
-	public void mouseDragged(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseMoved(MouseEvent e) {
-		gHook.x = e.getX();
-		gHook.y = e.getY();
-		
 	}
 
 }
--- src/fishsim/graphics/GameDisplay.java
+++ src/fishsim/graphics/GameDisplay.java
@@ -1,17 +1,35 @@
 package fishsim.graphics;
 
 import java.awt.Dimension;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 
 import engine.core.graphics.Display;
+import fishsim.board.GameBoard;
 
 public class GameDisplay extends Display {
 
 	private static final long serialVersionUID = -3314553873794350154L;
+	
+	public static int mouseX = 0, mouseY = 0;
 
 	public GameDisplay(Dimension dimensions, int scale, String title) {
 		super(dimensions, scale, title);
 		
-		addMouseMotionListener(GameBoard);
+		addMouseMotionListener(new MouseMotionListener() {
+			
+			@Override
+			public void mouseMoved(MouseEvent e) {
+				mouseX = e.getX();
+				mouseY = e.getY();
+			}
+			
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 	}
 
 }
